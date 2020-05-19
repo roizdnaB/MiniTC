7e06df5d4f4ea457d3fe8d2adfa8c1c318222550
roizdnaB <danieljambor1999@gmail.com>
2020-05-19 15:37:07 +0200

    Added ViewModel base

---
+++ MiniTC/ViewModel/Base/RelayCommand.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace MiniTC.ViewModel.Base
+{
+    class RelayCommand : ICommand // klasa imitująca polecenie
+    {
+        readonly Action<object> _execute;         // delegata metod nic nie zwracających o 1 argumencie t. object
+        readonly Predicate<object> _canExecute;   // delegata metod zwracających bool o argumencie t. object
+
+        public RelayCommand(Action<object> execute, Predicate<object> canExecute)
+        {
+            if (execute == null) throw new ArgumentNullException(nameof(execute));
+            else _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        // możliwość wykonania polecenia:
+        public bool CanExecute(object parameter) => _canExecute == null ? true : _canExecute(parameter);
+        public event EventHandler CanExecuteChanged
+        {
+            add { if (_canExecute != null) CommandManager.RequerySuggested += value; }    // dołączanie metody do zdarzenia
+            remove { if (_canExecute != null) CommandManager.RequerySuggested -= value; } // usuwanie metody ze zdarzenia
+        }
+
+        //wykonanie polecenia:
+        public void Execute(object parameter) { _execute(parameter); }
+    }
+}
---
+++ MiniTC/ViewModel/Base/ViewModelBase.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace MiniTC.ViewModel.Base
+{
+    class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void onPropertyChanged(params string[] namesOfProperties) //zmiana właściwości
+        {
+            if (PropertyChanged != null)
+            {
+                foreach (var prop in namesOfProperties)
+                { PropertyChanged(this, new PropertyChangedEventArgs(prop)); }
+            }
+        }
+    }
+}
